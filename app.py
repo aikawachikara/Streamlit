9b148ee8cf5d854cf34644e99e09e8a0048b4f87
A-Chikara <c-aikawa@you-me.co.jp>
2025-03-28 17:20:02 +0900

    up

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-# 背景を白に設定
+# ✅ 変更分を明示的に表示
+st.markdown("### ✅ 変更分：背景を白に設定")
+
+# 背景を白に設定（CSS）
 st.markdown("""
     <style>
     body {
@@ -16,12 +19,12 @@ st.markdown("# Head1")
 st.markdown("## Head2")
 st.markdown("----")
 
-check = st.checkbox("他のウィジェットを表示する")  # 引数に入れることでboolを返す
+check = st.checkbox("他のウィジェットを表示する")
 
 if check:
-   st.button("ボタン")  # 引数に入れるとboolで返す
-   st.selectbox("メニューリスト", ("選択肢1", "選択肢2", "選択肢3"))  # 第一引数：リスト名、第二引数：選択肢
-   st.multiselect("メニューリスト（複数選択可）", ("選択肢1", "選択肢2", "選択肢3"))  # 複数選択可
-   st.radio("ラジオボタン", ("選択肢1", "選択肢2", "選択肢3"))  # ラジオボタン
-   st.text_input("文字入力欄")  # テキスト入力欄
-   st.text_area("テキストエリア")  # テキストエリア
+    st.button("ボタン")
+    st.selectbox("メニューリスト", ("選択肢1", "選択肢2", "選択肢3"))
+    st.multiselect("メニューリスト（複数選択可）", ("選択肢1", "選択肢2", "選択肢3"))
+    st.radio("ラジオボタン", ("選択肢1", "選択肢2", "選択肢3"))
+    st.text_input("文字入力欄")
+    st.text_area("テキストエリア")
